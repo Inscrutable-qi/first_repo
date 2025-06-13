b2dd964b83d52e9f52f4dc13490ba3540cf9b0b7
Nachiketa Reddy <tnachiketareddy@gmail.com>
2025-06-13 08:59:37 +0530

    Smthg bday

---
+++ main.py
@@ -0,0 +1,12 @@
+import datetime ; import bday_messages 
+
+today = datetime.date.today()
+
+next_birthday = datetime.date(2008,1,29)
+
+days_away = (today - next_birthday)
+
+if today == next_birthday:
+    print(bday_messages.random_message)
+else:
+    print(f'My next birthday is {days_away.days} days away!')
